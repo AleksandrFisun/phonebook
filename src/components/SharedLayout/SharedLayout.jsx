ed9603822bfa00f5803cccffb1bc4eb0eeb021a1
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2023-05-28 15:08:40 +0300

    updates

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import AppBarr from 'components/AppBar/AppBar';
 
-import Loader from 'Loader/Loader';
+import Loader from 'loader/Loader';
 
 export const SharedLayout = () => {
   return (
--- src/components/phonebook/listContact/ListContact.jsx
+++ src/components/phonebook/listContact/ListContact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useGetPhoneBookQuery } from 'redux/services/contactsSlice';
 import ItemContact from 'components/Phonebook/itemContact/itemContact';
-import SpinnerBig from 'Loader/Spinner';
+import SpinnerBig from 'loader/Spinner';
 import {
   ListContactWrapper,
   List,
